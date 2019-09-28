5a6852cc5c11c1d09f107103020fb5eb2a688fe9
unknown <kamal@nvestbank.com>
2019-09-28 20:02:35 +0530

    frist

--- assets/h.js
+++ assets/h.js
@@ -287,3 +287,22 @@ varying float vBright;\n\
 		ready=1;
 	})
 //})()
+
+
+
+
+
+
+
+
+function scrollVideo() {
+	var video = $('#video').get(0),
+	  videoLength = video.duration,
+	  scrollPosition = $(document).scrollTop();
+	
+	video.currentTime = (scrollPosition / ($(document).height() - $(window).height())) * videoLength;
+  }
+  
+  $(window).scroll(function(e) {
+	scrollVideo();
+  });
